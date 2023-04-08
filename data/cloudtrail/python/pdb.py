ed7348297b10bb9fd2f45b2fc4627cecf58a1007
Matt Franz <mdfranz@gmail.com>
2023-04-08 19:41:16 +0000

    more queries and set threads to fix OOM Errors

--- data/cloudtrail/python/pdb.py
+++ data/cloudtrail/python/pdb.py
@@ -1,15 +1,27 @@
 #!/usr/bin/env python3
 
-import duckdb
+import duckdb,humanize
 import sys,psutil
 
 db = duckdb.connect()
+
+q = {}
+q['aws_svc_cnt'] = """select source.address, count(*) as cnt from aws_cloudtrail 
+                      where source.address like '%amazonaws.com' group by source.address order by cnt desc;"""
+q['infreq_events'] = """select event.action, count(*) as cnt from aws_cloudtrail 
+group by event.action order by cnt limit 15;"""
+
+db.execute('set threads to 4')
 
 if "view" not in sys.argv:
   db.execute('create table aws_cloudtrail as select * from "*.parquet"')
 else:
   db.execute('create view aws_cloudtrail as select * from "*.parquet"')
 
-print( db.execute('select count(*) from aws_cloudtrail').fetchall() )
-print( "Process Memory", psutil.Process().memory_info().rss )
-print( "Virtual Memory", psutil.virtual_memory() )
+print( humanize.intword(db.execute('select count(*) from aws_cloudtrail').fetchall()[0][0] ))
+
+for k,v in q.items():
+  print (f"=== {k} ===")
+  print( db.execute(v).fetchall())
+  print( "Process Memory", humanize.filesize.naturalsize( psutil.Process().memory_info().rss ))
+  print( "Virtual Memory", psutil.virtual_memory() )
